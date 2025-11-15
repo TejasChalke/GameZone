a178a0a486163c8a72dcc01ae57cf324186f9e99
Tejas Chalke <tejaschalke66@gmail.com>
2025-11-15 13:01:04 +0530

    initial commit

---
+++ src/test/java/com/_l/gamezone/GamezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com._l.gamezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
